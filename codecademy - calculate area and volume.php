38698bb5832cb9ecbb41c6bbd3191deb757c55d5
Pia Lopez Copetti <60473265+pwolff90@users.noreply.github.com>
2021-12-25 11:17:09 -0300

    codecademy - calculate area and volume.php
    Codecademy exercise to program a function that calculates area given number input, and same with volume in another function.

---
+++ codecademy - calculate area and volume.php
@@ -0,0 +1,13 @@
+<?php
+// Write your code below:
+function calculateArea($num_1, $num_2) {
+  return $num_1 * $num_2;
+}
+
+echo calculateArea(2, 6);
+
+function calculateVolume($height, $width, $depth) {
+  return $height * $width * $depth;
+}
+
+echo calculateVolume(2, 6, 9);
